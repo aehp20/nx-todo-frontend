d93bd16c722c12ce6e5faa0d757fdec508bd8714
Aron HERRERA PONTE <aronherrera2009@gmail.com>
2024-04-26 07:21:54 +0200

    improve tailwind.config.js to use safelist for components

---
+++ libs/design-system/src/lib/toggles/toggle.safelist.ts
@@ -0,0 +1,17 @@
+const safelist = [
+  'peer-disabled:bg-gray-300',
+  'peer-disabled:hover:bg-gray-300',
+  'peer-focus:shadow-[0_0_1px_Black]',
+  'hover:bg-MetallicSeaweedHover',
+  'peer-checked:hover:bg-MetallicSeaweedHover',
+  'bg-BeauBlue',
+  'before:bg-Alabaster',
+  'peer-checked:bg-MetallicSeaweed',
+  'bg-Caramel',
+  'peer-checked:bg-ChromeYellow',
+  'hover:bg-ChromeYellowHover',
+  'peer-checked:hover:bg-ChromeYellowHover',
+  'before:bg-Kurotobi',
+];
+
+exports.safelist = safelist;
--- libs/design-system/tailwind.config.js
+++ libs/design-system/tailwind.config.js
@@ -1,15 +1,10 @@
 const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
 const { join } = require('path');
 
+const toggle = require('./src/lib/toggles/toggle.safelist');
+
 const safelist = [
-  "peer-disabled:bg-gray-300",
-  "peer-disabled:hover:bg-gray-300",
-  "peer-focus:shadow-[0_0_1px_Black]",
-  "hover:bg-MetallicSeaweedHover",
-  "peer-checked:hover:bg-MetallicSeaweedHover",
-  "bg-BeauBlue",
-  "before:bg-Alabaster",
-  "peer-checked:bg-MetallicSeaweed"
+  ...toggle.safelist
 ];
 
 /** @type {import('tailwindcss').Config} */

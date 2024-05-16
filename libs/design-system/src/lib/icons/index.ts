c99ece7c6739c1624db3202e81c6f77daf8a51ba
Aron HERRERA PONTE <aronherrera2009@gmail.com>
2024-05-16 13:30:24 +0200

    add CloudXMarkIcon

--- libs/design-system/src/lib/icons/index.ts
+++ libs/design-system/src/lib/icons/index.ts
@@ -3,6 +3,7 @@ import { SVGProps } from 'react';
 export type SVGIcon = SVGProps<SVGSVGElement>;
 
 export { default as ClipboardCheckIcon } from './ClipboardCheckIcon';
+export { default as CloudXMarkIcon } from './CloudXMarkIcon';
 export { default as FastArrowLeftIcon } from './FastArrowLeftIcon';
 export { default as FastArrowRightIcon } from './FastArrowRightIcon';
 export { default as MenuIcon } from './MenuIcon';
